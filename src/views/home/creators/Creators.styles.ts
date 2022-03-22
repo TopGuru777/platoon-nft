f43ce66a0bd87c5440dfedda1e136e8966a04a96
Topguru777 <topguru21@gmail.com>
2022-03-23 01:27:14 +0800

    feat: creator part

--- src/views/home/creators/Creators.styles.ts
+++ src/views/home/creators/Creators.styles.ts
@@ -1,1 +1,49 @@
 import styled from "styled-components";
+
+export const CreatorWrapper = styled.div`
+  margin: 150px 0;
+  background-color: #282626;
+  padding: 5% 0;
+  h1 {
+    margin: 0;
+    margin-bottom: 5%;
+    text-align: center;
+    font-family: "Permanent Marker", cursive;
+    font-size: 44px;
+    line-height: 49px;
+    color: #fff;
+  }
+`;
+
+export const CreatorGridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-content: center;
+`;
+
+export const CreatorCardWrapper = styled.div`
+  width: 242px;
+  margin: auto;
+  h3,
+  h4,
+  p {
+    margin: 0;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 6px;
+    font-family: poppins-semibold, poppins, sans-serif;
+  }
+  h4 {
+    font-size: 19px;
+  }
+  h4 {
+    font-size: 17px;
+  }
+  p {
+    font-size: 14px;
+  }
+  h4,
+  p {
+    font-weight: 200;
+  }
+`;
